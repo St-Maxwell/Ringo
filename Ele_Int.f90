4f89c417b88f712be2a8d648268bee6b43dd09f5
St Maxwell <33542937+St-Maxwell@users.noreply.github.com>
2017-12-06 23:02:20 +0800

    Update Ele_Int.f90

--- Ele_Int.f90
+++ Ele_Int.f90
@@ -33,7 +33,7 @@ function overlap_gto(rab,a,b,La,Lb)
     implicit none
     real(kind=8) :: rab,a,b,La,Lb
     real(kind=8) :: overlap_gto
-    overlap_gto=La*Lb*(4.0D0*a*b)**0.75D0/(a+b)**1.5D0*exp(-a*b/(a+b)*rab**2.000000D0)
+    overlap_gto = La*Lb*(4.0D0*a*b)**0.75D0/(a+b)**1.5D0*exp(-a*b/(a+b)*rab**2.000000D0)
     return
 end function
 
@@ -42,7 +42,7 @@ function kinetic_gto(rab,a,b,La,Lb)
     real(kind=8) :: rab,a,b,La,Lb
     real(kind=8) :: kinetic_gto
     kinetic_gto = La*Lb*(2.828427125D0*(a*b)**1.750000D0/(a+b)**2.500000D0) & 
-                  & *(3.000000D0-2.000000D0*a*b/(a+b)*rab**2.000000D0*exp(-a*b/(a+b)*rab**2.000000D0)
+                  & *(3.000000D0-2.000000D0*a*b/(a+b)*rab**2.000000D0)*exp(-a*b/(a+b)*rab**2.000000D0)
     return
 end function
 
@@ -55,7 +55,7 @@ subroutine Nu_attracive_integral(coord,nu_charge,basis_a,basis_d,nu_attrac,n)
 	real(kind=8) :: coord(n,3),basis_a(n,3),basis_d(n,3)
 	real(kind=8) :: nu_attrac(n,n)
     real(kind=8) :: rab,rp(3),temp
-    real(kind=8),external :: Distance,overlap_gto,kinetic_gto
+    real(kind=8),external :: Distance,nu_attrac_gto
     !-----------------------------------------------------
     do i=1,n
         do j=i,n
@@ -96,8 +96,9 @@ subroutine E_E_Repulse(coord,basis_a,basis_d,two_e,n)
     implicit none
     integer :: n,i,j,k,l,p,q,r,s
     real(kind=8) :: coord(n,3),basis_a(n,3),basis_d(n,3)
-    real(kind=8) :: two_e(n,n)
+    real(kind=8) :: two_e(n,n,n,n)
     real(kind=8) :: rab,rcd,rp(3),rq(3),temp
+    real(kind=8),external :: Distance,two_e_gto
     !-----------------------------------------------------
     do i=1,n
         do j=i,n
@@ -112,7 +113,8 @@ subroutine E_E_Repulse(coord,basis_a,basis_d,two_e,n)
                                 do s=1,3
                                     rp=(basis_a(i,p)*coord(i,:)+basis_a(j,q)*coord(j,:))/(basis_a(i,p)+basis_a(j,q))
                                     rq=(basis_a(k,r)*coord(k,:)+basis_a(l,s)*coord(l,:))/(basis_a(k,r)+basis_a(l,s))
-                                    temp=temp+two_e_gto()
+                                    temp=temp+two_e_gto(rab,rcd,rp,rq,basis_a(i,p),basis_a(j,q),basis_a(k,r), &
+                                         & basis_a(l,s),basis_d(i,p),basis_d(j,q),basis_d(k,r),basis_d(l,s))
                                 end do
                             end do
                         end do
@@ -131,9 +133,10 @@ end subroutine
 function two_e_gto(rab,rcd,rp,rq,a,b,c,d,La,Lb,Lc,Ld)
     implicit none
     real(kind=8) :: rab,rcd,a,b,c,d,La,Lb,Lc,Ld
-    real(kind=8) :: rp(3),rq(3)
-    real(kind=8) :: rpq,temp
-    real(kind=8) :: BoysF
+    real(kind=8) :: rp(3),rq(3),rpq
+    real(kind=8),external :: BoysF
+    real(kind=8),parameter :: pi=3.1415926535897932385D+00
+    !-----------------------------------------------------
     rpq=Distance(rp,rq)
     two_elec = La*Lb*Lc*Ld*16.000000D0/dsqrt(pi)*(a*b*c*d)**0.750000D0/((a+b)*(c+d)*dsqrt(a+b+c+d)) &
                & * exp(-a*b/(a+b)*rab**2.000000D0-c*d/(c+d)*rcd**2.000000D0)*BoysF((a+b)*(c*d)/(a+b+c+d)*rpq**2.000000D0)
